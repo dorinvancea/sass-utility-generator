c1791307b3b8de1c0f24a3b988f7e4afe3e19fb3
Dorin Vancea <dorinvancea@users.noreply.github.com>
2025-02-22 07:33:42 +0000

    Sizes for base 16

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ module.exports = {
   files: [
     { src: 'src/sizes.scss', dest: 'dist/sizes.css' },
     { src: 'src/spacing.scss', dest: 'dist/spacing.css' },
+    { src: 'src/sizes-base16.scss', dest: 'dist/sizes-base16.css' },
   ],
 };
